bb2918990466bc8769d163f8a29a365f42ec76e1
yevhenii_husak <takerterik@gmail.com>
2025-05-03 13:57:12 +0200

    add

---
+++ src/scripts/generateContacts.js
@@ -0,0 +1,13 @@
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const contactsParse = await readContacts();
+  const newContacts = Array(number).fill(0).map(createFakeContact);
+  const result = [...contactsParse, ...newContacts];
+  console.log(result);
+  writeContacts(result);
+};
+
+generateContacts(5);
